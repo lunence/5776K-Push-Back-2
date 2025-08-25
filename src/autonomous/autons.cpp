f35fb53de85ca84184cb65f20058b3a18f641aeb
lunence <mooncyjoyli@gmail.com>
2025-08-24 19:31:41 -0700

    red swp (until center bottom)
    Co-authored-by: vnainika <vnainika@users.noreply.github.com>
    Co-authored-by: Niket223 <Niket223@users.noreply.github.com>

--- src/autonomous/autons.cpp
+++ src/autonomous/autons.cpp
@@ -8,7 +8,7 @@ void redSWP() {
     chassis.moveToPoint(8, 30.76, 500, {.minSpeed=127});
     chassis.waitUntilDone();
     intakeState = 5;
-    pros::delay(1500);
+    pros::delay(1000);
     intakeState = 0;
 
     //back away from match loader and turn to long goal
@@ -18,12 +18,21 @@ void redSWP() {
     chassis.turnToHeading(-90, 1000);
 
     //go to long goal and score
-    chassis.moveToPoint(-19, 30.5, 1000);
+    chassis.moveToPoint(-21, 30.5, 1000);
     chassis.waitUntilDone();
     intakeState = 1;
     pros::delay(1000);
 
     //go to middle right blocks
-    chassis.turnToHeading(209.2, 1000);
-    chassis.moveToPoint(-29.5, 9.5, 1000);
+    chassis.moveToPoint(-1, 30.5, 1000, {.forwards = false});
+    chassis.moveToPose( -28.2, 7.5, 274, 1000, {.lead = 0.4, .maxSpeed = 60});
+    chassis.moveToPoint(-36.4, 7.5, 1000, {.maxSpeed = 80});
+    pros::delay(2000);
+    intakeState = 0; 
+
+    //move to center bottom goal and score
+    chassis.turnToHeading(214.9, 1000);
+    chassis.moveToPoint(-44.2, -5, 1000);
+    chassis.waitUntilDone();
+    intakeState = 3;
 }
