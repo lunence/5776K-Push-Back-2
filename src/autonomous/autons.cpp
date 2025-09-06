1e11be174bb5882f58e7a133ce92c7b187401ce5
lunence <mooncyjoyli@gmail.com>
2025-09-05 22:18:37 -0700

    qw

--- src/autonomous/autons.cpp
+++ src/autonomous/autons.cpp
@@ -16,7 +16,7 @@ void redSWP() {
     intakeState = 5;
     leftMotors.move_voltage(1000);
     rightMotors.move_velocity(1000);
-    pros::delay(1200); //decrease
+    pros::delay(11200); //decrease
     leftMotors.move_voltage(0);
     rightMotors.move_voltage(0);
     intakeState = 0;
@@ -28,7 +28,7 @@ void redSWP() {
 
 
     //back away from match loader
-    chassis.moveToPoint(-7, 28.9, 1000, {.forwards = false});
+    chassis.moveToPoint(-7, 28.9, 1000, {.forwards = false, .maxSpeed = 80});
     chassis.waitUntilDone();
     littleWillState = 0;
 
