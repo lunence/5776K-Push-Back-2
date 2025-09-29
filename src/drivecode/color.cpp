dc2966b0d5c3f9b9761ff605dbf62c0c7ea6b452
Aashrita Koyyalamudi <aashrita.koyyalamudi@gmail.com>
2025-09-28 17:53:14 -0700

    rumblerumble

--- src/drivecode/color.cpp
+++ src/drivecode/color.cpp
@@ -2,53 +2,22 @@
 #include "pros/misc.h"
 
 
-bool colorPressed;
-bool colorDetected;
+//color sort RUMBLE 
+
+bool colorDetected = false;
+
 int color = 0;
 
-void updateColorSort() {
-    if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT)){
-        if(!colorPressed) { //if the button is pressed
-            color++; //add color state by increment of 1
-            if(color == 3) { //if 3, run fuction, 
-                color = 0; // off
-            }
-        }
-        colorPressed = true; //run function
-    } else {
-        colorPressed = false; //if not, dont run function
-    }
-}
-
-// function to update color sort (so like switch color from red to blue to no sort)
-
-
-//function to run color sort
-/*detect the color of the block using hue values.
-Red sort: extend the piston to push out red, keep blue
-Blue: extend piston to push out blue, keep red
-
-when the hue value is between a certain range (red)
-    extend the piston (set piston state)
-    retract piston immediately
-*/
-
-void runColorSort(){
-    while(true) {
-        if(color == 0) {
-            pros::delay(10);
-            continue;
-        }
+void updateColorSort(){
         
         if(color == 1) { //score red, sort blue
             if(200 < colorLeft.get_hue() && colorLeft.get_hue() < 240 || 200 < colorRight.get_hue() && colorRight.get_hue() < 240) {
                 if(!colorDetected) {
                     colorDetected = true;
+                    controller.rumble(". - . -");
                     
-                    colorPistonState = 1;
-                    colorPistonState = 0;
                 }
-                
+
             } 
             else {
                     colorDetected = false;
@@ -60,14 +29,87 @@ void runColorSort(){
             if(0 < colorLeft.get_hue() && colorLeft.get_hue() < 25 || 0 < colorRight.get_hue() && colorRight.get_hue() < 25) {
                 if(!colorDetected) {
                     colorDetected = true;
-                    
-                    colorPistonState = 1;
-                    colorPistonState = 0;
+                    controller.rumble("- . - .");
+                   
                 }
             }
-            else {
+
+            else 
+            {
                     colorDetected = false;
                 }
+
         }
-    }
+    
 }
+
+
+// bool colorPressed;
+// bool colorDetected;
+// int color = 0;
+
+// void updateColorSort() {
+//     if(controller.get_digital(pros::E_CONTROLLER_DIGITAL_LEFT)){
+//         if(!colorPressed) { //if the button is pressed
+//             color++; //add color state by increment of 1
+//             if(color == 3) { //if 3, run fuction, 
+//                 color = 0; // off
+//             }
+//         }
+//         colorPressed = true; //run function
+//     } else {
+//         colorPressed = false; //if not, dont run function
+//     }
+// }
+
+// // function to update color sort (so like switch color from red to blue to no sort)
+
+
+// //function to run color sort
+// /*detect the color of the block using hue values.
+// Red sort: extend the piston to push out red, keep blue
+// Blue: extend piston to push out blue, keep red
+
+// when the hue value is between a certain range (red)
+//     extend the piston (set piston state)
+//     retract piston immediately
+// */
+
+// void runColorSort(){
+//     while(true) {
+//         if(color == 0) {
+//             pros::delay(10);
+//             continue;
+//         }
+        
+//         if(color == 1) { //score red, sort blue
+//             if(200 < colorLeft.get_hue() && colorLeft.get_hue() < 240 || 200 < colorRight.get_hue() && colorRight.get_hue() < 240) {
+//                 if(!colorDetected) {
+//                     colorDetected = true;
+                    
+//                     colorPistonState = 1;
+//                     colorPistonState = 0;
+//                 }
+                
+//             } 
+//             else {
+//                     colorDetected = false;
+//                 }    
+        
+//         }
+            
+//         if(color == 2) { //score blue, sort red
+//             if(0 < colorLeft.get_hue() && colorLeft.get_hue() < 25 || 0 < colorRight.get_hue() && colorRight.get_hue() < 25) {
+//                 if(!colorDetected) {
+//                     colorDetected = true;
+                    
+//                     colorPistonState = 1;
+//                     colorPistonState = 0;
+//                 }
+//             }
+//             else {
+//                     colorDetected = false;
+//                 }
+//         }
+//     }
+// }
